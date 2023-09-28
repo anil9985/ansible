af13b17228c725a97f262517f682bbaf13d45878
anil9985 <146072555+anil9985@users.noreply.github.com>
2023-09-28 16:09:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
 provider "aws" {
   version = "~> 4.0"
   region     = "us-east-2"
-  access_key = "AKIAZWEYXR4JREADETNR"
-  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
 ami           = "ami-0d406e26e5ad4de53"
 instance_type = "t2.micro"
-key_name      = "masterkeypair"
 
  tags = {
-    Name = "example-server"
+    Name = "Server-Git-jenkins"
  }
 }
