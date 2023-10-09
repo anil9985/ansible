f3707f96a9b28612dea690bb78b7b50450cedbe4
anil9985 <146072555+anil9985@users.noreply.github.com>
2023-10-09 15:07:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,11 @@ instance_type = "t2.micro"
  tags = {
     Name = "Git-jenkins-1"
  }
+}
 output "private_ip"{   
      value = aws_instance.web.private_ip
 }
 output "public_ip"{
      value = aws_instance.web.public_ip
 }
-}
+
